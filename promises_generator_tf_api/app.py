e854694d7043ebec857b3a8ae23f4a12317e2472
Vinicius Bignardi <vbt.desenvolvimento@gmail.com>
2024-07-22 19:14:20 -0300

    Primeira versão da API para o Render

---
+++ promises_generator_tf_api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import json
+import random
+
+app = Flask(__name__)
+
+@app.route('/promessas', methods=['GET'])
+def get_random_promise():
+    with open('promessas.json', 'r', encoding='utf-8') as f:
+        promises = json.load(f)
+    random_promise = random.choice(promises)
+    return jsonify({'promessa': random_promise})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
