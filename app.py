4c2b79fc3d990d14efca4846e014406a05294dc0
Vinicius Bignardi <vbt.desenvolvimento@gmail.com>
2024-07-29 21:03:24 -0300

    Versão 3.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 import json
 import random
+import os
 
 app = Flask(__name__)
 
@@ -12,14 +13,20 @@ def index():
     versiculo = random_promise.get('versiculo', '')
     localizacao = random_promise.get('localizacao', '')
     combined_message = f"{versiculo} {localizacao}"
-    return render_template('index.html', promessa=combined_message)
+
+    # Seleciona uma imagem aleatória da pasta static/img
+    img_folder = os.path.join(app.static_folder, 'img')
+    img_files = os.listdir(img_folder)
+    random_img = random.choice(img_files)
+
+    return render_template('index.html', promessa=combined_message, imagem_fundo=random_img)
 
 @app.route('/promessas', methods=['GET'])
 def get_random_promise():
     with open('promessas.json', 'r', encoding='utf-8') as f:
         promises = json.load(f)
     random_promise = random.choice(promises)
-    return jsonify(random_promise)  # Retorna o JSON da promessa completa
+    return jsonify(random_promise)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
